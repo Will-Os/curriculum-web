0e9c91050b991e7355dfd1c635694bf3e2a60ca7
unknown <vicente.daniel.puebla@gmail.com>
2024-07-03 00:21:57 -0400

    Cambios Varios
    Se hicieron cambios varios en la UI, se añadió el botón de modo oscuro, se mejoró la velocidad de las animaciones de la sidebar, ahora la barra de opciones será siempre visible

--- main.js
+++ main.js
@@ -2,24 +2,25 @@ $(document).ready(function(){
     //Activacion de ToolTips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    var modoOscuro = false;
 
     //Funcionalidad Boton sidebar
     var sidebar = $(".sidebar");
     var contenido = $("#contenedor");
 
     $(".boton-sidebar").on("click",function(){
-        contenido.toggle("slide",{direction: "right"});
-        sidebar.toggle("slide",{direction: "left"});
+        sidebar.toggle("slide",{direction: "left"},"fast");
+        if($(window).width() < 680){
+            contenido.toggle("slide",{direction: "down"},"fast");
+        }
     });
 
     //Cambio de tamaños dinámico
     $(window).on("resize", function() {
         if ($(window).width() > 680) {
-            sidebar.show();
             contenido.show();
-        } else {
+        } else{
             sidebar.hide();
-            contenido.show();
         }
     });
 
@@ -33,4 +34,27 @@ $(document).ready(function(){
         var nombreTooltip = $(".mail").attr("aria-describedby");
         $("#"+nombreTooltip).find(".tooltip-inner").text("¡Correo Copiado!");
     });
+
+    //Modo Oscuro
+    $(".boton-modo").on("click",function(){
+        if(modoOscuro){
+            $("body").css("background-color","#efefef");
+            $(".contenido").css("color","#212529");
+            $(".delimitador").css("border-color","#500452c9");
+            $(".boton-modo").css("background-color","#303030");
+            $(".boton-modo").css("color","#efefef");
+            $(".bi-brightness-high-fill").addClass("bi-moon-stars-fill");
+            $(".bi-brightness-high-fill").removeClass("bi-brightness-high-fill");
+            modoOscuro = false;
+        }else{
+            $("body").css("background-color","#303030");
+            $(".contenido").css("color","#efefef");
+            $(".delimitador").css("border-color","#efefef");
+            $(".boton-modo").css("background-color","#efefef");
+            $(".boton-modo").css("color","#303030");
+            $(".bi-moon-stars-fill").addClass("bi-brightness-high-fill");
+            $(".bi-moon-stars-fill").removeClass("bi-moon-stars-fill");
+            modoOscuro = true;
+        }
+    })
 });
